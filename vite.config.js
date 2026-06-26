8ce86750687a3e3a7c49ab42a29fbed91fe65f60
Abdiel-Bonilla <abboro123@gmail.com>
2026-06-26 13:47:45 -0600

    Personal portafolio 2.1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Abdiel-Bonilla.github.io/',
+  base: '/Sign-Track/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
