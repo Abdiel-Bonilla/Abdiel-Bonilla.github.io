c40269a9f7f2f3656cae21685fe83107f9923fe0
Abdiel-Bonilla <abboro123@gmail.com>
2026-06-29 20:55:55 -0600

    Cambio de color

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -44,7 +44,7 @@ export const Hero = () => {
             {[...Array(30)].map((_, i) => (
                 <div className="absolute w-1.5 h-1.5 rounded-full opacity-60"
                 style={{
-                    backgroundColor: "#20B2A6",
+                    backgroundColor: "#3a86ff",
                     left: `${Math.random()* 100}%`,
                     top: `${Math.random()* 100}%`,
                     animation: `slow-drift ${15 + Math.random() * 20}s ease-in-out infinite`,
